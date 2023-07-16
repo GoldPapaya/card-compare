c35a0029b8e77451cfb289dc0ad60f1f0b54c799
GoldPapaya <lucasdcrawford32@gmail.com>
2023-07-16 00:27:39 -0400

    test commit

--- cardCompare.py
+++ cardCompare.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import random
 import math
 
+# Test commit
 # Note that several variables are lists solely for matplotlib
 num_simulations = 100
 max_num_comparisons = 1000
